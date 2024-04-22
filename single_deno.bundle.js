bd88729cc241929e54b6ac35665c93e5ea22f0ab
Marxu <fuhaojun@52tt.com>
2024-04-22 16:27:26 +0800

    feat: deno 另一个非常强大的特性(单一可执行文件（世界上任何一台电脑都可运行✅）) 使用Deno工具将JavaScript或TypeScript代码打包成一个单一的可执行文件。这个可执行文件包含了应用程序的所有代码和依赖，用户可以在没有任何其他依赖项的情况下运行它

---
+++ single_deno.bundle.js
@@ -0,0 +1,8 @@
+// deno-fmt-ignore-file
+// deno-lint-ignore-file
+// This code was bundled using `deno bundle` and it's not recommended to edit it manually
+
+function greet(name) {
+    console.log(`Hello, ${name}!`);
+}
+greet("World");
---
+++ single_deno.js
@@ -0,0 +1,8 @@
+// 定义一个简单的函数
+function greet(name) {
+    console.log(`Hello, ${name}!`);
+  }
+  
+  // 调用函数
+  greet("World");
+  
